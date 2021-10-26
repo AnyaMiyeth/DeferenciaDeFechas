0dc883771080c64f2fd470b69f371c1fac053485
anyamiyeth <anyamiyeth@byasystems.com.co>
2021-10-26 00:24:46 -0500

    Agregar archivos de proyecto.

---
+++ DeferenciaDeFechas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DeferenciaDeFechas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                DateTime fechaInicio = DateTime.Now;
+                DateTime fechaFin = fechaInicio.AddDays(31);
+                var diferenciaFechas = fechaFin - fechaInicio;
+                var diasTrascurridos = diferenciaFechas.Days;
+                decimal mesesFraccionTranscurrdios = diasTrascurridos / 30m;
+                var mes = Math.Ceiling(mesesFraccionTranscurrdios);
+
+
+                Console.WriteLine($"Fecha Inicio {fechaInicio}");
+                Console.WriteLine($"Fecha Fin {fechaFin}");
+                Console.WriteLine($"Mes o fraccion {mesesFraccionTranscurrdios}");
+                Console.WriteLine($"Meses {mes}");
+                Console.ReadKey();
+            
+        }
+    }
+}
